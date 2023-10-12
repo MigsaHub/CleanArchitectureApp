9f2b32b7877aa3254e96378417d4682e44fcac9d
MigsaHub <miglianis@gmail.com>
2023-10-12 16:51:26 -0300

    Add project files.

---
+++ CleanArchitectureApp/Controllers/VoidController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoidController : ControllerBase
+    {
+    }
+}
