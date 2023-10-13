b4a23e9df2829a340bc042e42d6de1ad3a078a25
MigsaHub <miglianis@gmail.com>
2023-10-13 11:39:52 -0300

    initial TDD

---
+++ CleanArchitectureApp.Tests/Controllers/PrescriptionControllerTest.cs
@@ -0,0 +1,30 @@
+﻿namespace CleanArchitectureApp.Tests.Controllers
+{
+    public class PrescriptionControllerTest
+    {
+        [Fact]
+        public void CreatePrescription_WhenRequestisOk_Successfull()
+        {
+
+        }
+
+        [Fact]
+        public void GetPrescriptionById_WhenRequestisOk_Successfull()
+        {
+
+        }
+
+        [Fact]
+        public void UpdatePrescriptionById_WhenRequestisOk_Successfull()
+        {
+
+        }
+
+
+        [Fact]
+        public void DeletePrescriptionById_WhenRequestisOk_Successfull()
+        {
+
+        }
+    }
+}
---
+++ CleanArchitectureApp.Tests/Controllers/UserControllerTest.cs
@@ -0,0 +1,79 @@
+﻿namespace CleanArchitectureApp.Tests.Controllers
+{
+    public class UserControllerTest
+    {
+        [Fact]
+        public async Task RegisterUser_ThrowsException()
+        {
+            //Arrange
+            IUserServiceRepository userServiceRepository = new MockUserRepository();
+
+            var userDto = new UserDto();
+            var service = new UserService(userServiceRepository);
+            //Act
+            //Assert 
+            await Assert.ThrowsAnyAsync<UserServiceException>(async () => await service.Register(userDto));
+        }
+
+
+        [Fact]
+        public void LoginUser_WhenRequestIsOk_Successfull()
+        {
+
+        }
+
+        [Fact]
+        public void UpdateUser_WhenRequestIsOk_Successfull()
+        {
+
+        }
+
+        [Fact]
+        public void DeleteUser_WhenRequestIsOk_Successfull()
+        {
+
+        }
+ 
+    }
+    public class MockUserRepository : IUserServiceRepository
+    {
+        public Task<bool> AddUser(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IUserServiceRepository
+    {
+        public Task<bool> AddUser(UserDto userDto);
+    }
+
+    public class UserServiceException : Exception
+    {
+    }
+
+    public class UserService
+    {  
+        private readonly IUserServiceRepository _userServiceRepository;
+        public UserService(IUserServiceRepository? userServiceRepository)
+        {
+            _userServiceRepository = userServiceRepository;
+        }
+
+        public async Task<bool> Register(UserDto userDto)
+        {
+           var result= await _userServiceRepository.AddUser(userDto);
+           return result;
+        }
+    }
+
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public UserDto() { }
+    }
+}
