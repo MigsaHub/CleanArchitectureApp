4bbf09d7120a8d536b049ee845d269f5104913bc
MigsaHub <miglianis@gmail.com>
2023-10-13 14:15:30 -0300

    Usercontroller register endpoint

---
+++ CleanArchitectureApp/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitectureApp.Application.DTO;
+using CleanArchitectureApp.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService) 
+        {
+          _userService = userService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<bool>> Register(UserDto request)
+        {
+            var result = await _userService.Register(request);
+            if (!result)
+                return BadRequest(result);
+            return Ok(result);
+        }
+    }
+}
